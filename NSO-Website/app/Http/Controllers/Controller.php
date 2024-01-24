1d89b55b19bcb51d1f2782a71a25f45388312f03
HeDoesCode <ethan.john0722@gmail.com>
2024-01-24 15:51:41 +0800

    branch push test

--- NSO-Website/app/Http/Controllers/Controller.php
+++ NSO-Website/app/Http/Controllers/Controller.php
@@ -9,4 +9,5 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+    // this is a comment, just to check if this branch is working.
 }
