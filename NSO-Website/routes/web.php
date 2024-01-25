7131c9bde1e3569e882e535ee90cd50eee4d018a
HeDoesCode <ethan.john0722@gmail.com>
2024-01-25 09:44:32 +0800

    added admin and featured products tables, models, controllers, and page

--- NSO-Website/routes/web.php
+++ NSO-Website/routes/web.php
@@ -1,7 +1,9 @@
 <?php
+require __DIR__.'/auth.php';
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FeaturedProductsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,4 +30,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/admin/featuredproducts', [FeaturedProductsController::class, 'featuredProducts'])->name('admin.featuredProducts');
