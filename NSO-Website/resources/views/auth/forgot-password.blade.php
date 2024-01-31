98a3ca3cd19ecb7a07b4fe20e87ac8000e419548
Simowwn <125945116+Simowwn@users.noreply.github.com>
2024-01-31 22:07:30 +0800

    forgot-password front end

--- NSO-Website/resources/views/auth/forgot-password.blade.php
+++ NSO-Website/resources/views/auth/forgot-password.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <div class="fp-form">
+    <div class="fp-form - col">
         <div >
             {{ __('Forgot your password? No problem. Just let us know your username and we will email you a password reset link to your registered email that will allow you to choose a new one.') }}
         </div>
@@ -11,18 +11,21 @@
             @csrf
     
             <!-- Username -->
-        <div>
-        <x-input-label for="username" :value="__('Username')" />
-        <x-text-input id="username"  type="text" name="username" :value="old('username')" required autofocus />
-        <x-input-error :messages="$errors->get('username')" class="mt-2" />
-        </div>
-    
-    
             <div>
-                <x-primary-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-primary-button>
+                <div>
+                    <x-input-label for="username" :value="__('Username')" />
+                    <x-text-input id="username"  type="text" name="username" :value="old('username')" required autofocus />
+                    <x-input-error :messages="$errors->get('username')" class="mt-2" />
+                    </div>
+                
+                
+                        <div>
+                            <x-primary-button class="fp-btn-reset">
+                                {{ __('Email Password Reset Link') }}
+                            </x-primary-button>
+                        </div>
             </div>
+       
         </form>
 
     </div>
