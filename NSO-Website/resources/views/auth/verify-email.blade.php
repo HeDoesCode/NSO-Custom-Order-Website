7b8d2aa5f00d1e95782a5e492ec1e2b653cc2016
Raphael Salazar <127420583+pngRMS@users.noreply.github.com>
2024-01-31 22:50:23 +0800

    verify-email-page modified#2

--- NSO-Website/resources/views/auth/verify-email.blade.php
+++ NSO-Website/resources/views/auth/verify-email.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <section class="verify_email_content">
     <div class="containerforgotpassword titlebar">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+       <p>{{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}</p> 
     </div>
     </section>
     <section class="verification">
@@ -12,12 +12,12 @@
     @endif
     </section>
     <section class="ve_button_log">
-    <div class="mt-4 flex row">
+    <div class="flex row items-center">
         <form method="POST" action="{{ route('verification.send') }}">
             @csrf
 
-            <div class="resend-button col">
-                <x-primary-button>
+            <div class="resend-button">
+                <x-primary-button class="resend-button-text">
                     {{ __('Resend Verification Email') }}
                 </x-primary-button>
             </div>
