dcc929e886647584745754731a9d7ba8f1d82ce8
Simowwn <125945116+Simowwn@users.noreply.github.com>
2024-01-31 21:47:38 +0800

    forgot-pass css

--- NSO-Website/resources/views/auth/forgot-password.blade.php
+++ NSO-Website/resources/views/auth/forgot-password.blade.php
@@ -1,26 +1,29 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your username and we will email you a password reset link to your registered email that will allow you to choose a new one.') }}
-    </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Username -->
-    <div>
-    <x-input-label for="username" :value="__('Username')" />
-    <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus />
-    <x-input-error :messages="$errors->get('username')" class="mt-2" />
-    </div>
-
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+    <div class="fp-form">
+        <div >
+            {{ __('Forgot your password? No problem. Just let us know your username and we will email you a password reset link to your registered email that will allow you to choose a new one.') }}
         </div>
-    </form>
+    
+        <!-- Session Status -->
+        <x-auth-session-status  :status="session('status')" />
+    
+        <form method="POST" action="{{ route('password.email') }}">
+            @csrf
+    
+            <!-- Username -->
+        <div>
+        <x-input-label for="username" :value="__('Username')" />
+        <x-text-input id="username"  type="text" name="username" :value="old('username')" required autofocus />
+        <x-input-error :messages="$errors->get('username')" class="mt-2" />
+        </div>
+    
+    
+            <div>
+                <x-primary-button>
+                    {{ __('Email Password Reset Link') }}
+                </x-primary-button>
+            </div>
+        </form>
+
+    </div>
 </x-guest-layout>
