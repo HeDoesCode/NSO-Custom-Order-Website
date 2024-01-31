868a114fe7861adf201ca79f2192bf743da96590
Simowwn <125945116+Simowwn@users.noreply.github.com>
2024-01-31 21:24:24 +0800

    verify-email

--- NSO-Website/resources/views/auth/verify-email.blade.php
+++ NSO-Website/resources/views/auth/verify-email.blade.php
@@ -6,7 +6,7 @@
     @if (session('status') == 'verification-link-sent')
         <div class="mb-4 font-medium text-sm text-green-600">
             {{ __('A new verification link has been sent to the email address you provided during registration.') }}
-        </div>
+        </div> 
     @endif
 
     <div class="mt-4 flex items-center justify-between">
