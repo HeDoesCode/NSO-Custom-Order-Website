d10d11cde92e0c57ba4f75c0af1272c2a317e20e
SeraphLaur <laurence.arcila@gmail.com>
2024-02-18 20:59:45 +0800

    tt

--- NSO-Website/resources/views/dashboard.blade.php
+++ NSO-Website/resources/views/dashboard.blade.php
@@ -2,34 +2,56 @@
     <div class="col-span-10 p-5">
         <div class="ml-6">
             <p class="text-3xl font-bold mb-7">
-                Order Dashboard
+                My Custom Orders
             </p>
         </div>
 
+        <a href="{{ route('order.create') }}" class="ml-6 text-white bg-gray-500 px-4 py-2 rounded-md mb-4 inline-block">+ Create Order</a>
+  
         @if ($orders)
             <table class="table-fixed w-full bg-white shadow-md rounded-lg overflow-hidden text-center">
                 <thead class="bg-gray-300">
                     <tr>
-                        <th class="px-4 py-2">Ordered By</th>
-                        <th class="px-4 py-2">Address</th>
-                        <th class="px-4 py-2">Mode of Payment</th>
+                        <th class="px-4 py-2">Design Image</th>
+                        <th class="px-4 py-2">Design Text</th>
                         <th class="px-4 py-2">Price</th>
                         <th class="px-4 py-2">Status</th>
-                        <th class="px-4 py-2">Action</th>
+                        <th class="px-4 py-2">Order Date</th>
+                        <th class="px-4 py-2">View</th>
+                        <th class="px-4 py-2">Feedback</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($orders as $order)
                         <tr class="even:bg-gray-100 -5">
-                            <td class="py-5">{{ $order->username }}</td>
-                            <td class="py-5">{{ $order->deliveryAddress }}</td>
-                            <td class="py-5">{{ $order->mode_of_payment }}</td>
-                            <td class="py-5">{{ $order->price ? '₱' . $order->price : "Not Yet Set" }}</td>
+                            <td class="px-4 py-5 flex justify-center">
+                                <div class="w-40 h-40 flex items-center justify-center">
+                                    @if ($order->design_img)
+                                        <img src="{{ asset('images/order design images/'.$order->design_img) }}"
+                                             class="w-full h-full object-cover object-center">
+                                    @else
+                                        <span class="text-gray-500">Image not available</span>
+                                    @endif
+                                </div>
+                            </td>
+                            <td class="py-5">{{ $order->design_text }}</td>
+                            <td class="py-5">{{ $order->price ? '₱' . $order->price : "Waiting For Seller" }}</td>
                             <td class="py-5">
                                 <span >{{ $order->status }}</span>
                             </td>
+                            <td class="py-5">{{ $order->order_date }}</td>
+                            
                             <td class="py-5 font-bold underline underline-offset-2">
                                 <a href="{{ route('admin.orderdetails', ['id' => $order->id]) }}">View Details</a>
+                            </td>
+                            <td>
+                                @if ($order->status == 'Order Completed' && !$order->feedback) <!-- Check status and if feedback is not given -->
+                                    <a href="{{ route('feedback.create') }}" class="ml-6 text-white bg-green-400 px-4 py-2 rounded-md mb-4 inline-block">Create Feedback</a>
+                                @else
+                                    <button class="ml-6 text-gray-400 bg-gray-200 px-4 py-2 rounded-md mb-4 inline-block" disabled>
+                                        Feedback Not Available
+                                    </button>
+                                @endif
                             </td>
                         </tr>
                     @endforeach
@@ -43,5 +65,5 @@
     @if (session()->has('success')) 
         <span>{{ session('success') }}</span>
     @endif
-    <a href="{{ route('order.create') }}"><button>+ Create Order</button></a>
+    
 </x-app-layout>
