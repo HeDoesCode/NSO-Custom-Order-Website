0359e271d668715246cc09f20036b6f7c81ffd88
Redvinz <arvinalkuino123@gmail.com>
2024-05-29 23:39:30 +0800

    Update chatbot.blade.php

--- NSO-Website/resources/views/layouts/chatbot.blade.php
+++ NSO-Website/resources/views/layouts/chatbot.blade.php
@@ -42,8 +42,7 @@ function closeForm() {
  -->
 
 
- <!--Start of Tawk.to Script-->
-<script type="text/javascript">
+<!-- <script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -53,5 +52,16 @@ s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
+</script> -->
+
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/665744f4981b6c5647762ff0/1hv2eamsq';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
 </script>
-<!--End of Tawk.to Script-->
